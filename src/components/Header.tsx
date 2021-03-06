afb9c8ae6a4026624c88a21e9321bae87b77636f
Melvin Beemer <melvin@hey.com>
2021-03-06 20:13:13 +0700

    Add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,15 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <div className="py-20 text-center text-4xl font-bold text-black dark:text-white">
-      <Link href="/">ZilStream</Link>
+    <div className="bg-white dark:bg-gray-800 mb-4 py-3 px-4 flex items-center text-black dark:text-white">
+      <Link href="/">
+        <a className="flex items-center justify-center">
+          <picture style={{width: '110px', height: '30px'}}>
+            <source srcSet="/logo-dark.svg" media="(prefers-color-scheme: dark)" />
+            <img src="/logo.svg" />
+          </picture> 
+        </a>
+      </Link>
     </div>
   )
 }
