18a3c9981b4e12ddad892354b1d309e7f63b1706
Melvin Beemer <melvin@hey.com>
2021-03-07 12:26:10 +0700

    Greatly improved homepage

--- src/components/ChartBlock.tsx
+++ src/components/ChartBlock.tsx
@@ -11,6 +11,7 @@ const Chart = dynamic(
 interface Props {
   token: Token,
   rates: Rate[],
+  zilRate: Rate,
 }
 
 const RatesBlock = (props: Props) => {
@@ -18,23 +19,28 @@ const RatesBlock = (props: Props) => {
   const lastRate = sortedRates.length > 0 ? sortedRates[0].value : 0
   const firstRate = sortedRates.length > 0 ? sortedRates[sortedRates.length-1].value : 0
   const lastRateRounded = (lastRate > 1) ? Math.round(lastRate * 100) / 100 : Math.round(lastRate * 10000) / 10000
+  const usdRate = lastRate * props.zilRate.value
 
   const change = ((lastRate - firstRate) / firstRate) * 100
   const changeRounded = Math.round(change * 100) / 100
   
   return (
     <div className="h-64 rounded-lg overflow-hidden p-2 shadow-md bg-white dark:bg-gray-800 text-black dark:text-white relative">
-      <div className="absolute top-3 left-4 right-4 flex items-center text-xl">
-        <div className="flex-grow flex items-center">
-          <span className="font-semibold mr-2">{props.token.symbol}</span>
-          <span>{lastRateRounded}</span>
+      <Chart data={props.rates} isIncrease={change >= 0} isUserInteractionEnabled={false} isScalesEnabled={false} />
+      <div className="z-10 absolute top-0 left-0 right-0 bottom-0 p-4">
+        <div className="flex items-center text-xl">
+          <div className="flex-grow flex items-center">
+            <span className="font-semibold mr-2">{props.token.symbol}</span>
+            <span className="mr-2">{lastRateRounded}</span>
+          </div>
+          <div className={change >= 0 ? 'text-green-600 dark:text-green-500' : 'text-red-600 dark:text-red-500'}>
+            {changeRounded} %
+          </div>
         </div>
-        <div className={change >= 0 ? 'text-green-600 dark:text-green-500' : 'text-red-600 dark:text-red-500'}>
-          {changeRounded} %
+        <div>
+          <span className="text-gray-400">{usdRate.toFixed(2)} USD</span>
         </div>
       </div>
-      <Chart data={props.rates} isIncrease={change >= 0} isUserInteractionEnabled={false} isScalesEnabled={false} />
-      <div className="z-10 absolute top-0 left-0 right-0 bottom-0"></div>
     </div>
   )
 }
