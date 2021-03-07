8ae7c1291024d593da743e63a7cd73948fd5101e
Melvin Beemer <melvin@hey.com>
2021-03-07 11:22:10 +0700

    Add footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import Head from 'next/head'
+import Footer from './Footer'
 
 interface Props {
   children: React.ReactNode
@@ -17,6 +18,7 @@ export default function Layout(props: Props) {
       <div className="flex flex-col h-full">
         <Header />
         <div className="px-4 my-4 flex-grow">{children}</div>
+        <Footer />
       </div>
     </>
   )
