d3795415b34cff9a1ca9530dafca7e7481a1e115
Melvin Beemer <melvin@hey.com>
2021-03-08 20:58:12 +0700

    Better table formatting

--- src/components/TokenRow.tsx
+++ src/components/TokenRow.tsx
@@ -29,14 +29,14 @@ const TokenRow = (props: Props) => {
 
   return (
     <div className="token-row">
-      <div className="w-6 mr-3"><img src={props.token.icon} loading="lazy" /></div>
-      <div className="w-24 md:w-48">{props.token.symbol}</div>
-      <div className="w-24 md:w-32 lg:w-40 font-normal">{lastRateRounded}</div>
-      <div className="w-32 lg:w-40 hidden md:block font-normal">{currencyFormat(usdRate)}</div>
-      <div className={change >= 0 ? 'text-green-600 dark:text-green-500 font-normal w-24 md:w-32 lg:w-40' : 'text-red-600 dark:text-red-500 font-normal w-24 md:w-32 lg:w-40'}>
+      <div className="w-6 mr-4"><img src={props.token.icon} loading="lazy" /></div>
+      <div className="w-20 sm:w-24 md:w-36">{props.token.symbol}</div>
+      <div className="w-24 md:w-28 lg:w-36 font-normal text-right">{lastRateRounded}</div>
+      <div className="w-32 lg:w-40 hidden md:block font-normal text-right">{currencyFormat(usdRate)}</div>
+      <div className={change >= 0 ? 'text-green-600 dark:text-green-500 font-normal w-24 md:w-32 lg:w-40 text-right' : 'text-red-600 dark:text-red-500 font-normal w-24 md:w-32 lg:w-40 text-right'}>
         {changeRounded} %
       </div>
-      <div className="w-32 lg:w-40 hidden lg:block font-normal">{currencyFormat(marketCap)}</div>
+      <div className="w-36 lg:w-44 xl:w-52 hidden lg:block font-normal text-right">{currencyFormat(marketCap)}</div>
       <div className="flex-grow flex justify-end">
         <div className="w-20 md:w-28 lg:w-36 h-16">
           <Chart data={props.rates} isIncrease={change >= 0} isUserInteractionEnabled={false} isScalesEnabled={false} />
