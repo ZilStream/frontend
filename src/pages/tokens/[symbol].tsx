18beb633dde4837906eb1ea25c4b0bb085a6bd73
Melvin Beemer <melvin@hey.com>
2021-03-14 23:35:24 +0700

    Detail progress

--- src/pages/tokens/[symbol].tsx
+++ src/pages/tokens/[symbol].tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import dynamic from 'next/dynamic'
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
 import { Rate } from 'shared/rate.interface'
 import { Token } from 'shared/token.interface'
-import { currencyFormat } from 'utils/format'
+import { currencyFormat, numberFormat, cryptoFormat } from 'utils/format'
+import { Link, FileText, Box, ExternalLink } from 'react-feather'
 
 const Candles = dynamic(
   () => import('components/Candles'),
@@ -36,6 +37,8 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
 }
 
 function TokenDetail({ token, rates, dailyRates, zilRates }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [hoveringAddress, setHoveringAddress] = useState(false);
+
   const sortedRates = dailyRates.sort((a,b) => (a.time < b.time) ? 1 : -1)
   const lastRate = sortedRates.length > 0 ? sortedRates[0].value : 0
   const firstRate = sortedRates.length > 0 ? sortedRates[sortedRates.length-1].value : 0
@@ -52,7 +55,7 @@ function TokenDetail({ token, rates, dailyRates, zilRates }: InferGetServerSideP
   return (
     <>
       <div className="flex flex-col md:flex-row items-stretch md:items-center">
-        <div className="flex-grow flex flex-col md:flex-row items-start md:items-center mb-4 pt-8 pb-2">
+        <div className="flex-grow flex flex-col md:flex-row items-start md:items-center mb-1 pt-8 pb-2">
           <div className="bg-gray-300 dark:bg-gray-800 w-16 h-16 p-2 rounded-lg mb-2 md:mb-0 mr-0 md:mr-3">
             <img src={token.icon} loading="lazy" />
           </div>
@@ -60,37 +63,67 @@ function TokenDetail({ token, rates, dailyRates, zilRates }: InferGetServerSideP
             <h2>{token.name}</h2>
             <div className="flex flex-col md:flex-row items-start md:items-center">
               <span className="text-gray-600 dark:text-gray-400 text-lg mr-3">${token.symbol}</span>
-              <span className="text-sm bg-gray-300 dark:bg-gray-800 dark:text-gray-400 rounded px-2">
-                <a href={`https://viewblock.io/zilliqa/address/${token.address_bech32}`} className="font-normal">
+              <div className="flex items-center">
+                <button 
+                  onMouseEnter={() => setHoveringAddress(true)}
+                  onMouseLeave={() => setHoveringAddress(false)}
+                  onClick={() => navigator.clipboard.writeText(token.address_bech32)}
+                  className="text-sm bg-gray-300 dark:bg-gray-800 dark:text-gray-400 rounded px-2 mr-2">
                   {token.address_bech32}
-                </a>
-              </span>
+                </button>
+                {hoveringAddress &&
+                  <span className="text-xs text-gray-600 transition">Click to copy</span>
+                }
+              </div>
             </div>
           </div>
         </div>
         <div className="text-left md:text-right font-medium flex items-center md:block mb-2 md:mb-0">
-          <div className="flex-grow font-semibold text-2xl">{lastRateRounded} ZIL</div>
+          <div className="flex-grow font-semibold text-2xl">{cryptoFormat(lastRateRounded)} ZIL</div>
           <div className={change >= 0 ? 'text-green-600 dark:text-green-500' : 'text-red-600 dark:text-red-500'}>
             {changeRounded} %
           </div>
         </div>
       </div>
-      <div className="py-2 mb-6 grid grid-cols-4 gap-4">
-        <div className="px-4 py-2 shadow-md bg-white dark:bg-gray-800 rounded-lg">
+      <div className="flex items-center text-sm text-gray-400">
+        {token.website &&
+          <a href={token.website} target="_blank" className="flex items-center bg-gray-800 hover:bg-gray-700 px-2 py-1 rounded mr-2">
+            <Link size={12} className="mr-1" />
+            Website 
+            <ExternalLink size={10} className="ml-1 text-gray-600" />
+          </a>
+        }
+        
+        {token.whitepaper &&
+          <a href={token.whitepaper} target="_blank" className="flex items-center bg-gray-800 hover:bg-gray-700 px-2 py-1 rounded mr-2">
+            <FileText size={12} className="mr-1" />
+            Whitepaper
+            <ExternalLink size={10} className="ml-1 text-gray-600" />
+          </a>
+        }
+        
+        <a href={`https://viewblock.io/zilliqa/address/${token.address_bech32}`} target="_blank" className="flex items-center bg-gray-800 hover:bg-gray-700 px-2 py-1 rounded mr-2">
+          <Box size={12} className="mr-1" />
+          ViewBlock 
+          <ExternalLink size={10} className="ml-1 text-gray-600" />
+        </a>
+      </div>
+      <div className="py-2 mb-6 grid grid-cols-4">
+        <div className="pr-4 py-2 border-r border-gray-800">
           <div className="text-gray-400">Market Cap</div>
           <div>{currencyFormat(marketCap)}</div>
         </div>
-        <div className="px-4 py-2 shadow-md bg-white dark:bg-gray-800 rounded-lg">
+        <div className="px-4 py-2 border-r border-gray-800">
           <div className="text-gray-400">Volume (24h)</div>
           <div>{currencyFormat(usdVolume)}</div>
         </div>
-        <div className="px-4 py-2 shadow-md bg-white dark:bg-gray-800 rounded-lg">
+        <div className="px-4 py-2 border-r border-gray-800">
           <div className="text-gray-400">Circulating Supply</div>
-          <div>{token.current_supply}</div>
+          <div>{numberFormat(token.current_supply, 0)}</div>
         </div>
-        <div className="px-4 py-2 shadow-md bg-white dark:bg-gray-800 rounded-lg">
+        <div className="pl-4 py-2">
           <div className="text-gray-400">Max Supply</div>
-          <div>10000000</div>
+          <div>{numberFormat(token.max_supply, 0)}</div>
         </div>
       </div>
       <div className="flex items-center justify-end mb-2">
