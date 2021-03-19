d46c87074a98876e086b5e8d9768a8ad5b49c55e
Melvin Beemer <melvin@hey.com>
2021-03-19 16:53:00 +0700

    Make ZilSwap logo clickable

--- src/components/ExchangeStats.tsx
+++ src/components/ExchangeStats.tsx
@@ -11,7 +11,9 @@ const ExchangeStats = (props: Props) => {
     <div className="mt-4 lg:mt-0 w-full md:w-auto bg-gray-300 dark:bg-gray-800 bg-opacity-75 dark:bg-opacity-100 rounded-lg px-5 py-2 text-gray-600 dark:text-gray-300 flex items-center text-xs sm:text-sm">
       <div className="flex-grow">
         <div className="flex items-center w-20 sm:w-24 mr-4 md:mr-6">
-          <img src={`https://zilstream.sgp1.cdn.digitaloceanspaces.com/zilswap.svg`} loading="lazy" />
+          <a href="https://zilswap.io/" target="_blank" className="block w-full h-full">
+            <img src={`https://zilstream.sgp1.cdn.digitaloceanspaces.com/zilswap.svg`} loading="lazy" />
+          </a>
         </div>
       </div>
       <div className="flex-grow flex items-center justify-end">
