cbbdf09a65b5781ca1e87581fadc86a30632acfc
Melvin Beemer <melvin@hey.com>
2021-03-21 14:24:22 +0700

    Update front page graphs every minute

--- src/lib/zilstream/getTokens.ts
+++ src/lib/zilstream/getTokens.ts
@@ -1,6 +1,6 @@
 import { Token } from "types/token.interface";
 
 export default async function getTokens(): Promise<Token[]> {
-  const res = await fetch(`${process.env.BACKEND_URL}/tokens`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/tokens`)
   return await res.json()
 }
