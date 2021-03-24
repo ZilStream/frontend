e2f8e407ddeb79a130c26fccc4b57fc1e7d22de2
Melvin Beemer <melvin@hey.com>
2021-03-24 09:47:06 +0700

    Fixed an issue where due to SSR icons would always render in its light version at first

--- src/components/TokenIcon.tsx
+++ src/components/TokenIcon.tsx
@@ -1,12 +1,17 @@
 import { useTheme } from 'next-themes'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface Props {
   url: string
 }
 
 const TokenIcon = (props: Props) => {
+  const [mounted, setMounted] = useState(false);
   const { resolvedTheme } = useTheme()
+
+  useEffect(() => setMounted(true), [])
+
+  if(!mounted) return null
 
   return (
     <img src={`${props.url}${(resolvedTheme == 'dark' ? '?t=dark' : '')}`} loading="lazy" />
