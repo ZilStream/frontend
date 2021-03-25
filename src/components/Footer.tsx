5b04f2526a864852e5162b34d3db28ab888869c5
Melvin Beemer <melvin@hey.com>
2021-03-25 18:32:11 +0700

    Add telegram as an icon in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,24 @@
 import Link from 'next/link'
 import React from 'react'
-import { GitHub, Twitter } from 'react-feather'
+import { GitHub, Twitter, Send } from 'react-feather'
 
 export default function Footer() {
   return (
     <div className="px-4 py-12 text-gray-500 dark:text-gray-400 text-sm text-center">
       <div className="font-medium mb-2">ZilStream</div>
       <div className="flex items-center justify-center text-gray-400">
-        <a href="https://github.com/ZilStream" className="mr-2">
-          <GitHub size={18} />
+        <a href="https://t.me/zilstream" className="mr-3">
+          <Send size={18} />
         </a>
-        <a href="https://twitter.com/zilstream">
+        <a href="https://twitter.com/zilstream" className="mr-3">
           <Twitter size={18} />
+        </a>
+        <a href="https://github.com/ZilStream">
+          <GitHub size={18} />
         </a>
       </div>
       <div className="mt-6 text-xs text-gray-400 dark:text-gray-500">
         <div className="mb-1"><Link href="/disclaimer"><a className="hover:underline">Disclaimer</a></Link></div>
-        <div className="mb-1">Telegram: <a href="https://t.me/zilstream" target="_blank">t.me/zilstream</a></div>
         <div>Tip jar: <span className="font-medium">zil1me2dr2za36mdxy0uygfx3s5eh9xckd227efqdx</span></div>
       </div>
     </div>
