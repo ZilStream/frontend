501aff9f8f8de9096dcf1489b8c3a18a37dce35a
Melvin Beemer <melvin@hey.com>
2021-03-28 02:43:29 +0700

    Fixed currency formatting for small numbers

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,15 +1,20 @@
 export function currencyFormat(num: number, symbol: string = "$"): string {
+  if(num < 0.1) {
+    return symbol + num.toFixed(5)
+  }
   return symbol + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
 
 export function numberFormat(num: number, decimals: number = 2): string {
+  if(num < 0.1) {
+    return num.toFixed(decimals)
+  }
   return num.toFixed(decimals).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
 
 export function cryptoFormat(num: number): string {
-  var decimals = 2
-  if(num < 0) {
-    decimals = 5
+  if(num < 0.1) {
+    return num.toFixed(5)
   }
-  return num.toFixed(decimals).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  return num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
