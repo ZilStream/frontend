7637895943bcf3809507450be9b4eef1693a137c
Melvin Beemer <melvin@hey.com>
2021-05-03 11:24:12 +0700

    Make sure to end connecting if zilpay is not installed

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -14,13 +14,17 @@ const ConnectWallet = (props: Props) => {
     const zilPay = (window as any).zilPay
     
     // Check if ZilPay is installed
-    if(typeof zilPay === "undefined")
+    if(typeof zilPay === "undefined") {
       console.log("ZilPay extension not installed")
-    
+      return
+    }
+      
     const result = await zilPay.wallet.connect()
 
-    if(result !== zilPay.wallet.isConnect)
+    if(result !== zilPay.wallet.isConnect) {
       console.log("Could not connect to ZilPay")
+      return
+    }
 
     const walletAddress = zilPay.wallet.defaultAccount.bech32
     const network = zilPay.wallet.net
