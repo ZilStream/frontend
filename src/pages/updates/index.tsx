6e3a0394382cc272ef33f2caca9f73494b18184a
Melvin Beemer <melvin@hey.com>
2021-05-06 15:09:31 +0700

    Fix updates overview margins

--- src/pages/updates/index.tsx
+++ src/pages/updates/index.tsx
@@ -19,10 +19,10 @@ function Updates() {
             height={385}
             className="rounded-lg"
           />
-          <h1 className="mt-20 text-2xl mb-1">Announcing Premium Membership</h1>
+          <h1 className="mt-4 text-2xl mb-1">Announcing Premium Membership</h1>
           <p>Introducing ZilStream Premium Membership, Portfolio and STREAM token.</p>
           <Link href="/updates/announcing-premium-membership">
-            <a className="font-normal underline">Read more</a>
+            <a className="block mt-1 font-normal underline">Read more</a>
           </Link>
         </div>
       </div>
