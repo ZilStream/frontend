b299eb716558e572fc51c40dfd1ea62660bfb67e
Melvin Beemer <melvin@hey.com>
2021-06-14 11:26:28 +0700

    Fix market bar on mobile

--- src/components/MarketBar.tsx
+++ src/components/MarketBar.tsx
@@ -27,31 +27,33 @@ const MarketBar = () => {
   return (
     <div className="flex items-center text-xs bg-white dark:bg-gray-800 border-t dark:border-gray-900">
       <div className="container px-3 md:px-4 py-2 flex items-center">
-        <div className="flex items-center flex-grow">
-          <div className="flex items-center mr-4">
-            <span className="text-gray-400 mr-2">ZIL:</span>
-            <span>${moneyFormat(tokenState.zilRate, {maxFractionDigits: 3})}</span>
-          </div>
-          <div className="flex items-center mr-4">
-            <span className="text-gray-400 mr-2">Market Cap:</span>
-            <span>{currencyFormat(marketCap)}</span>
-          </div>
-          <div className="flex items-center mr-4">
-            <span className="text-gray-400 mr-2">TVL:</span>
-            <Link href="/liquidity">
-              <a className="font-normal">
-                <span>{currencyFormat(liquidity * tokenState.zilRate)}</span>
-                <span className="mx-1">-</span>
-                <span>{currencyFormat(liquidity, '')} ZIL</span>
-              </a>
-            </Link>
-          </div>
-          <div className="flex items-center">
-            <span className="text-gray-400 mr-2">Volume (24h):</span>
-            <span>{currencyFormat(volume * tokenState.zilRate)}</span>
+        <div className="overflow-x-auto flex-grow scrollable-table-container">
+          <div className="flex items-center" style={{minWidth: '740px'}}>
+            <div className="flex items-center mr-4">
+              <span className="text-gray-400 mr-2">ZIL:</span>
+              <span>${moneyFormat(tokenState.zilRate, {maxFractionDigits: 3})}</span>
+            </div>
+            <div className="flex items-center mr-4">
+              <span className="text-gray-400 mr-2">Market Cap:</span>
+              <span>{currencyFormat(marketCap)}</span>
+            </div>
+            <div className="flex items-center mr-4">
+              <span className="text-gray-400 mr-2">TVL:</span>
+              <Link href="/liquidity">
+                <a className="font-normal">
+                  <span>{currencyFormat(liquidity * tokenState.zilRate)}</span>
+                  <span className="mx-1">-</span>
+                  <span>{currencyFormat(liquidity, '')} ZIL</span>
+                </a>
+              </Link>
+            </div>
+            <div className="flex items-center">
+              <span className="text-gray-400 mr-2">Volume (24h):</span>
+              <span>{currencyFormat(volume * tokenState.zilRate)}</span>
+            </div>
           </div>
         </div>
-        <div className="items-center hidden sm:flex">
+        <div className="items-center flex flex-shrink-0 ml-3">
           <button  
             onClick={() => setTheme(resolvedTheme == 'dark' ? 'light' : 'dark')}
             className="rounded-full text-gray-400 hover:text-black dark:hover:text-white focus:outline-none">
