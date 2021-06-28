3a19098e0fb9679011b2a87ae6bf050438bff142
Melvin Beemer <melvin@hey.com>
2021-06-29 00:15:34 +0700

    Add volume ema30

--- src/pages/liquidity.tsx
+++ src/pages/liquidity.tsx
@@ -4,7 +4,7 @@ import { InferGetServerSidePropsType } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import React from 'react'
-import { cryptoFormat, currencyFormat } from 'utils/format'
+import { cryptoFormat, currencyFormat, numberFormat } from 'utils/format'
 import useMoneyFormatter from 'utils/useMoneyFormatter'
 
 export const getServerSideProps = async () => {
@@ -53,9 +53,9 @@ const Liquidity = ({ stats }: InferGetServerSidePropsType<typeof getServerSidePr
           <thead className="text-gray-500 dark:text-gray-400 text-xs">
             <tr>
               <th className="pl-3 pr-2 py-2 text-left">Token</th>
-              <th className="px-2 py-2 text-right">Volume</th>
               <th className="px-2 py-2 text-right">Reserve</th>
               <th className="px-2 py-2 text-right">Liquidity</th>
+              <th className="px-2 py-2 text-right">Volume (EMA30)</th>
               <th className="px-2 py-2 text-right">EMA30</th>
               <th className="pl-2 pr-3 py-2 text-right">Rewards Tier</th>
             </tr>
@@ -85,7 +85,6 @@ const Liquidity = ({ stats }: InferGetServerSidePropsType<typeof getServerSidePr
                       </a>
                     </Link>
                   </td>
-                  <td className="px-2 py-2 font-normal text-right">{currencyFormat(token.volume_24h)}</td>
                   <td className="px-2 py-2 font-normal text-right">
                     <div>{cryptoFormat(token.zil_reserve)} <span className="font-medium">ZIL</span></div>
                     <div>{cryptoFormat(token.token_reserve)} <span className="font-medium">{token.symbol}</span></div>
@@ -94,8 +93,11 @@ const Liquidity = ({ stats }: InferGetServerSidePropsType<typeof getServerSidePr
                     <div>{currencyFormat(token.liquidity)}</div>
                     <div>{cryptoFormat(token.liquidity_zil)} <span className="font-medium">ZIL</span></div>
                   </td>
+                  <td className="px-2 py-2 font-normal text-right">
+                    {numberFormat(token.volume_ema30_zil, 0)} <span className="font-medium">ZIL</span>
+                  </td>
                   <td className={`px-2 py-2 font-normal text-right`}>
-                  {cryptoFormat(token.liquidity_ema30_zil)}
+                    {numberFormat(token.liquidity_ema30_zil, 0)} <span className="font-medium">ZIL</span>
                   </td>
                   <td className={`pl-2 pr-3 py-2 text-right ${index === 0 ? 'rounded-tr-lg' : ''} ${index === tokens.length-1 ? 'rounded-br-lg' : ''}`}>
                     {tier}
