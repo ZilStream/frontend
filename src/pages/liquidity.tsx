9a6e16c478784e810cc6fce8b6e18f8ee9afbed4
Melvin Beemer <melvin@hey.com>
2021-07-21 10:42:42 +0700

    Added score

--- src/pages/liquidity.tsx
+++ src/pages/liquidity.tsx
@@ -49,6 +49,7 @@ const Liquidity = ({ stats }: InferGetServerSidePropsType<typeof getServerSidePr
             <col style={{width: '140px', minWidth: 'auto'}} />
             <col style={{width: '140px', minWidth: 'auto'}} />
             <col style={{width: '140px', minWidth: 'auto'}} />
+            <col style={{width: '140px', minWidth: 'auto'}} />
           </colgroup>
           <thead className="text-gray-500 dark:text-gray-400 text-xs">
             <tr>
@@ -57,6 +58,7 @@ const Liquidity = ({ stats }: InferGetServerSidePropsType<typeof getServerSidePr
               <th className="px-2 py-2 text-right">Liquidity</th>
               <th className="px-2 py-2 text-right">Volume (EMA30)</th>
               <th className="px-2 py-2 text-right">Liquidity (EMA30)</th>
+              <th className="px-2 py-2 text-right">Score</th>
               <th className="pl-2 pr-3 py-2 text-right">Rewards Tier</th>
             </tr>
           </thead>
@@ -72,6 +74,13 @@ const Liquidity = ({ stats }: InferGetServerSidePropsType<typeof getServerSidePr
               } else if(token.liquidity_ema30_zil > 500000) {
                 tier = <span className="bg-blue-400 py-1 px-2 rounded font-medium">Tier 3</span>
               }
+
+              const score = Math.min(
+                5, 
+                Math.sqrt(token.liquidity_ema30_zil) / 10000000,
+                (token.volume_ema30_zil / token.liquidity_ema30_zil) / 0.03
+              )
+
               return (
                 <tr key={token.address} role="row" className="text-sm border-b dark:border-gray-700 last:border-b-0 whitespace-nowrap">
                   <td className={`pl-4 pr-2 py-4 flex items-center font-medium ${index === 0 ? 'rounded-tl-lg' : ''} ${index === tokens.length-1 ? 'rounded-bl-lg' : ''}`}>
@@ -98,6 +107,9 @@ const Liquidity = ({ stats }: InferGetServerSidePropsType<typeof getServerSidePr
                   </td>
                   <td className={`px-2 py-2 font-normal text-right`}>
                     {numberFormat(token.liquidity_ema30_zil, 0)} <span className="font-medium">ZIL</span>
+                  </td>
+                  <td className={`px-2 py-2 font-normal text-right`}>
+                    {score}
                   </td>
                   <td className={`pl-2 pr-3 py-2 text-right ${index === 0 ? 'rounded-tr-lg' : ''} ${index === tokens.length-1 ? 'rounded-br-lg' : ''}`}>
                     {tier}
