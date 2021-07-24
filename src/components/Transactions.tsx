8181d3b00546d082583cd2c7e246300c04bea246
Melvin Beemer <melvin@hey.com>
2021-07-24 18:01:11 +0700

    Fix mobile transactions layout

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -33,19 +33,30 @@ const TransactionsGroup = (props: Props) => {
           const outToken = findToken(transaction.token_out_address)
           
           return (
-            <div className="px-4 py-3 border-b dark:border-gray-700 last:border-b-0 flex items-center">
-              <div className="w-16 flex">
-                <div className="bg-gray-100 dark:bg-gray-700 p-3 rounded-full">
-                  <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" className="transactions-type" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M131.3 231.1L32 330.6l99.3 99.4v-74.6h174.5v-49.7H131.3v-74.6zM480 181.4L380.7 82v74.6H206.2v49.7h174.5v74.6l99.3-99.5z"></path></svg>
+            <div className="px-4 py-3 border-b dark:border-gray-700 last:border-b-0 flex flex-col md:flex-row md:items-center text-sm md:text-base">
+              <div className="md:w-64 flex items-center">
+                <div className="flex mr-2 md:mr-6">
+                  <div className="bg-gray-100 dark:bg-gray-700 h-6 w-6 md:w-10 md:h-10 p-1 md:p-3 rounded-full flex items-center justify-center">
+                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" className="transactions-type" xmlns="http://www.w3.org/2000/svg"><path d="M131.3 231.1L32 330.6l99.3 99.4v-74.6h174.5v-49.7H131.3v-74.6zM480 181.4L380.7 82v74.6H206.2v49.7h174.5v74.6l99.3-99.5z"></path></svg>
+                  </div>
+                </div>
+                <div className="flex-grow mr-4 flex items-center md:items-start md:flex-col">
+                  <div className="mr-1">Swap</div>
+                  <time className="text-sm text-gray-500">{dayjs(transaction.timestamp).format('h:mm a')}</time>
+                </div>
+                <div className="text-sm text-gray-500 dark:text-gray-300 block md:hidden">
+                  <a 
+                    href={`https://viewblock.io/zilliqa/tx/0x${transaction.hash}`} 
+                    target="_blank" 
+                    className="flex items-center py-1 rounded font-normal">
+                    ViewBlock 
+                    <ExternalLink size={12} className="ml-2 text-gray-500 dark:text-gray-300" />
+                  </a>
                 </div>
               </div>
-              <div className="w-48 mr-4 flex flex-col">
-                <div>Swap</div>
-                <time className="text-sm text-gray-500">{dayjs(transaction.timestamp).format('h:mm a')}</time>
-              </div>
-              <div className="flex-grow flex items-center">
+              <div className="flex-grow flex items-center mt-1 md:mt-0">
                 <div className="w-48 flex items-center">
-                  <div className="bg-gray-100 dark:bg-gray-700 rounded-full w-8 h-8 mr-2" style={{padding: '5px'}}>
+                  <div className="bg-gray-100 dark:bg-gray-700 rounded-full w-8 h-8 flex items-center justify-center mr-2" style={{padding: '5px'}}>
                     <TokenIcon address={transaction.token_in_address} />
                   </div>
                   <div className="font-medium">
@@ -59,7 +70,7 @@ const TransactionsGroup = (props: Props) => {
                   <ArrowRight size={18} className="text-gray-500" />
                 </div>
                 <div className="w-48 flex items-center">
-                  <div className="bg-gray-100 dark:bg-gray-700 rounded-full w-8 h-8 mr-2" style={{padding: '5px'}}>
+                  <div className="bg-gray-100 dark:bg-gray-700 rounded-full w-8 h-8 flex items-center justify-center mr-2" style={{padding: '5px'}}>
                     <TokenIcon address={transaction.token_out_address} />
                   </div>
                   <div className="font-medium">
@@ -70,7 +81,7 @@ const TransactionsGroup = (props: Props) => {
                   </div>
                 </div>
               </div>
-              <div className="text-sm text-gray-500 dark:text-gray-300">
+              <div className="hidden md:block text-sm text-gray-500 dark:text-gray-300">
                 <a 
                   href={`https://viewblock.io/zilliqa/tx/0x${transaction.hash}`} 
                   target="_blank" 
@@ -86,15 +97,26 @@ const TransactionsGroup = (props: Props) => {
         if(transaction.sub_type === 'add_liquidity') {
           const token = findToken(transaction.token_in_address)
           return (
-            <div className="px-4 py-3 border-b dark:border-gray-700 last:border-b-0 flex items-center">
-              <div className="w-16 flex">
-                <div className="bg-gray-100 dark:bg-gray-700 p-3 rounded-full">
-                  <ArrowUp size={16} />
+            <div className="px-4 py-3 border-b dark:border-gray-700 last:border-b-0 flex flex-col md:flex-row md:items-center text-sm md:text-base">
+              <div className="md:w-64 flex items-center">
+                <div className="flex mr-2 md:mr-6">
+                  <div className="bg-gray-100 dark:bg-gray-700 h-6 w-6 md:w-10 md:h-10 p-1 md:p-3 rounded-full flex items-center justify-center">
+                    <ArrowUp size={16} />
+                  </div>
                 </div>
-              </div>
-              <div className="w-48 mr-4 flex flex-col">
-                <div>Liquidity</div>
-                <time className="text-sm text-gray-500">{dayjs(transaction.timestamp).format('h:mm a')}</time>
+                <div className="flex-grow mr-4 flex items-center md:items-start md:flex-col">
+                  <div className="mr-1">Liquidity</div>
+                  <time className="text-sm text-gray-500">{dayjs(transaction.timestamp).format('h:mm a')}</time>
+                </div>
+                <div className="text-sm text-gray-500 dark:text-gray-300 block md:hidden">
+                  <a 
+                    href={`https://viewblock.io/zilliqa/tx/0x${transaction.hash}`} 
+                    target="_blank" 
+                    className="flex items-center py-1 rounded font-normal">
+                    ViewBlock 
+                    <ExternalLink size={12} className="ml-2 text-gray-500 dark:text-gray-300" />
+                  </a>
+                </div>
               </div>
               <div className="flex-grow flex items-center">
                 <div className="w-48 flex flex-col font-medium">
@@ -136,7 +158,7 @@ const TransactionsGroup = (props: Props) => {
                   </div>
                 </div>
               </div>
-              <div className="text-sm text-gray-500 dark:text-gray-300">
+              <div className="hidden md:block text-sm text-gray-500 dark:text-gray-300">
                 <a 
                   href={`https://viewblock.io/zilliqa/tx/0x${transaction.hash}`} 
                   target="_blank" 
@@ -156,23 +178,34 @@ const TransactionsGroup = (props: Props) => {
           }
     
           return (
-            <div className="px-4 py-3 border-b dark:border-gray-700 last:border-b-0 flex items-center">
-              <div className="w-16 flex">
-                <div className="bg-gray-100 dark:bg-gray-700 p-3 rounded-full">
-                  {incoming ? (
-                    <ArrowDown size={16} />
-                  ) : (
-                    <ArrowUp size={16} />
-                  )}
+            <div className="px-4 py-3 border-b dark:border-gray-700 last:border-b-0 flex flex-col md:flex-row md:items-center text-sm md:text-base">
+              <div className="md:w-64 flex items-center">
+                <div className="flex mr-2 md:mr-6">
+                  <div className="bg-gray-100 dark:bg-gray-700 h-6 w-6 md:w-10 md:h-10 p-1 md:p-3 rounded-full flex items-center justify-center">
+                    {incoming ? (
+                      <ArrowDown size={16} />
+                    ) : (
+                      <ArrowUp size={16} />
+                    )}
+                  </div>
                 </div>
-              </div>
-              <div className="w-48 mr-4 flex flex-col">
-                {incoming ? (
-                  <div>Receive</div>
-                ) : (
-                  <div>Send</div>
-                )}
-                <time className="text-sm text-gray-500">{dayjs(transaction.timestamp).format('h:mm a')}</time>
+                <div className="flex-grow mr-4 flex items-center md:items-start md:flex-col">
+                  {incoming ? (
+                    <div className="mr-1">Receive</div>
+                  ) : (
+                    <div className="mr-1">Send</div>
+                  )}
+                  <time className="text-sm text-gray-500">{dayjs(transaction.timestamp).format('h:mm a')}</time>
+                </div>
+                <div className="text-sm text-gray-500 dark:text-gray-300 block md:hidden">
+                  <a 
+                    href={`https://viewblock.io/zilliqa/tx/0x${transaction.hash}`} 
+                    target="_blank" 
+                    className="flex items-center py-1 rounded font-normal">
+                    ViewBlock 
+                    <ExternalLink size={12} className="ml-2 text-gray-500 dark:text-gray-300" />
+                  </a>
+                </div>
               </div>
               <div className="flex-grow flex items-center">
                 <div className="w-48 flex flex-col font-medium">
@@ -214,7 +247,7 @@ const TransactionsGroup = (props: Props) => {
                   </div>
                 </div>
               </div>
-              <div className="text-sm text-gray-500 dark:text-gray-300">
+              <div className="hidden md:block text-sm text-gray-500 dark:text-gray-300">
                 <a 
                   href={`https://viewblock.io/zilliqa/tx/0x${transaction.hash}`} 
                   target="_blank" 
