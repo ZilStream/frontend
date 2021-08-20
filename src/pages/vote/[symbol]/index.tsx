807528f97a8c13686a554e33e9f3263ca4e5630e
Melvin Beemer <melvin@hey.com>
2021-08-20 20:49:19 +0700

    Fix heads for governance voting

--- src/pages/vote/[symbol]/index.tsx
+++ src/pages/vote/[symbol]/index.tsx
@@ -6,6 +6,7 @@ import dayjs from 'dayjs'
 import getGovernanceProposals from 'lib/zilliqa/getGovernanceProposals'
 import getGovernanceSpaces from 'lib/zilliqa/getGovernanceSpaces'
 import { useRouter } from 'next/dist/client/router'
+import Head from 'next/head'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import { Proposal } from 'types/proposal.interface'
@@ -40,43 +41,51 @@ function VoteTokenDetail() {
   }, [symbol])
 
   return (
-    <div className="max-w-5xl mx-auto">
-      {space ? (
-        <div className="flex items-center gap-3 pt-8 pb-4">
-          <div className="w-16 h-16 bg-gray-300 dark:bg-gray-800 p-2 rounded-lg"><TokenIcon address={space?.token} /></div>
-          <div className="flex flex-col">
-            <div className="font-semibold">{space?.name}</div>
-            <div>{space?.symbol}</div>
+    <>
+      <Head>
+        <title>{space?.symbol.toUpperCase()} proposals | ZilStream</title>
+        <meta property="og:title" content={`${space?.symbol.toUpperCase()} proposals | ZilStream`} />
+        <meta name="description" content={`${space?.symbol.toUpperCase()} governance proposals`} />
+        <meta property="og:description" content={`${space?.symbol.toUpperCase()} governance proposals`} />
+      </Head>
+      <div className="max-w-5xl mx-auto">
+        {space ? (
+          <div className="flex items-center gap-3 pt-8 pb-4">
+            <div className="w-16 h-16 bg-gray-300 dark:bg-gray-800 p-2 rounded-lg"><TokenIcon address={space?.token} /></div>
+            <div className="flex flex-col">
+              <div className="font-semibold">{space?.name}</div>
+              <div>{space?.symbol}</div>
+            </div>
           </div>
-        </div>
-      ) : (
-        <LoadingSpaceHeader />
-      )}
-      
-      {proposals.length > 0 ? (
-        <div className="bg-white dark:bg-gray-800 px-5 rounded-lg">
-          {proposals.map((proposal, index) => {
-            return (
-              <div key={index} className="py-4 border-b dark:border-gray-700 last:border-b-0">
-                <div><Link href={`/vote/${symbol}/${proposal.authorIpfsHash}`}><a className="font-medium">{proposal.msg.payload.name}</a></Link></div>
-                <div className="flex items-center gap-2">
-                  <div className="text-gray-500 dark:text-gray-400">Starts {dayjs.unix(proposal.msg.payload.start).format("MM-DD-YYYY")}, ends {dayjs.unix(proposal.msg.payload.end).format("MM-DD-YYYY")}</div>
-                  {space?.members.includes(toBech32Address(proposal.address)) &&
-                    <div className="text-sm bg-gray-200 dark:bg-gray-600 px-2 rounded-full">Core</div>
-                  }
+        ) : (
+          <LoadingSpaceHeader />
+        )}
+        
+        {proposals.length > 0 ? (
+          <div className="bg-white dark:bg-gray-800 px-5 rounded-lg">
+            {proposals.map((proposal, index) => {
+              return (
+                <div key={index} className="py-4 border-b dark:border-gray-700 last:border-b-0">
+                  <div><Link href={`/vote/${symbol}/${proposal.authorIpfsHash}`}><a className="font-medium">{proposal.msg.payload.name}</a></Link></div>
+                  <div className="flex items-center gap-2">
+                    <div className="text-gray-500 dark:text-gray-400">Starts {dayjs.unix(proposal.msg.payload.start).format("MM-DD-YYYY")}, ends {dayjs.unix(proposal.msg.payload.end).format("MM-DD-YYYY")}</div>
+                    {space?.members.includes(toBech32Address(proposal.address)) &&
+                      <div className="text-sm bg-gray-200 dark:bg-gray-600 px-2 rounded-full">Core</div>
+                    }
 
-                  {dayjs.unix(proposal.msg.payload.start).isBefore(dayjs()) && dayjs.unix(proposal.msg.payload.end).isAfter(dayjs()) &&
-                    <div className="text-sm bg-primary dark:bg-primaryDark px-2 rounded-full">Active</div>
-                  }
+                    {dayjs.unix(proposal.msg.payload.start).isBefore(dayjs()) && dayjs.unix(proposal.msg.payload.end).isAfter(dayjs()) &&
+                      <div className="text-sm bg-primary dark:bg-primaryDark px-2 rounded-full">Active</div>
+                    }
+                  </div>
                 </div>
-              </div>
-            )
-          })}
-        </div>
-      ) : (
-        <LoadingProposals />
-      )}
-    </div>
+              )
+            })}
+          </div>
+        ) : (
+          <LoadingProposals />
+        )}
+      </div>
+    </>
   )
 }
 
--- src/pages/vote/index.tsx
+++ src/pages/vote/index.tsx
@@ -4,6 +4,7 @@ import { Space } from 'types/space.interface'
 import TokenIcon from 'components/TokenIcon'
 import Link from 'next/link'
 import LoadingSpaces from 'components/LoadingSpaces'
+import Head from 'next/head'
 
 function Vote() {
   const [spaces, setSpaces] = useState<Space[]>([])
@@ -23,18 +24,26 @@ function Vote() {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6 mt-8">
-      {spaces.map(space => {
-        return (
-          <Link key={space.token} href={`/vote/${space.symbol.toLowerCase()}`}>
-            <a className="bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-lg flex flex-col px-4 py-10 items-center">
-              <div className="w-16 h-16"><TokenIcon address={space.token} /></div>
-              <div className="mt-4 font-semibold">{space.name}</div>
-            </a>
-          </Link>
-        )
-      })}
-    </div>
+    <>
+      <Head>
+        <title>Vote | ZilStream</title>
+        <meta property="og:title" content={`Vote | ZilStream`} />
+        <meta name="description" content={`Vote on governance proposals`} />
+        <meta property="og:description" content={`Vote on governance proposals`} />
+      </Head>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6 mt-8">
+        {spaces.map(space => {
+          return (
+            <Link key={space.token} href={`/vote/${space.symbol.toLowerCase()}`}>
+              <a className="bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-lg flex flex-col px-4 py-10 items-center">
+                <div className="w-16 h-16"><TokenIcon address={space.token} /></div>
+                <div className="mt-4 font-semibold">{space.name}</div>
+              </a>
+            </Link>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
