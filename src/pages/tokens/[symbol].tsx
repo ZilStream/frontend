87d1d0d897a32377ee228597c74ef8ad0c9b35a4
Melvin Beemer <melvin@hey.com>
2021-10-06 16:35:09 +0700

    Fix native zil apr on token page

--- src/pages/tokens/[symbol].tsx
+++ src/pages/tokens/[symbol].tsx
@@ -65,7 +65,7 @@ function TokenDetail({ token }: InferGetServerSidePropsType<typeof getServerSide
       const rewardTokens = tokenState.tokens.filter(token => token.address_bech32 == reward.reward_token_address)
       if(rewardTokens.length > 0) {
         const rewardToken = rewardTokens[0]
-        const rewardsValue = toBigNumber(reward.amount).times(rewardToken.rate).times(tokenState.zilRate)
+        const rewardsValue = reward.reward_token_symbol !== 'ZIL' ? toBigNumber(reward.amount).times(rewardToken.rate).times(tokenState.zilRate) : toBigNumber(reward.amount).times(rewardToken.rate)
         const liquidity = toBigNumber(reward.adjusted_total_contributed_share).times(token.market_data.current_liquidity)
         const roiPerEpoch = rewardsValue.dividedBy(liquidity)
         const apr = bnOrZero(roiPerEpoch.times(52).shiftedBy(2).decimalPlaces(1))
