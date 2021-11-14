b6b2cfa2433419c9708ef98f7f0ce6eb5f0f7c00
Melvin Beemer <melvin@hey.com>
2021-11-14 09:57:42 +0700

    Change to triangles and fix hidden

--- src/components/InlineChange.tsx
+++ src/components/InlineChange.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ChevronDown, ChevronUp } from 'react-feather'
+import { ChevronDown, ChevronUp, Triangle } from 'react-feather'
 import { numberFormat } from 'utils/format'
 
 interface Props {
@@ -15,11 +15,11 @@ const InlineChange = (props: Props) => {
   const isNegative = num < 0
   return <span className={`inline-flex items-center ${props.bold && 'font-semibold'} ${isPositive && 'text-green-600 dark:text-green-500'}  ${isNegative && 'text-red-600 dark:text-red-500'} ${props.className ?? ""}`}>
     {isPositive &&
-      <ChevronUp size={14} strokeWidth={3} />
+      <Triangle size={7} strokeWidth={3} className="fill-current mr-1" />
     }
 
     {isNegative &&
-      <ChevronDown size={14} strokeWidth={3} />
+      <Triangle size={7} strokeWidth={3} className="fill-current transform rotate-180 mr-1" />
     }
     
     {numberFormat(Math.abs(num))}%
--- src/components/TokenLiquidity.tsx
+++ src/components/TokenLiquidity.tsx
@@ -103,8 +103,8 @@ const TokenLiquidity = (props: Props) => {
             <div key={holder.address} className="flex items-center text-gray-800 dark:text-gray-300 py-2 border-b last:border-b-0 dark:border-gray-700">
               <div className="flex-grow flex items-center">
                 <div className="w-10 text-gray-500 dark:text-gray-400">{index+1}.</div>
-                <div className="none md:block">{holder.address}</div>
-                <div className="block md:none">{shortenAddress(holder.address)}</div>
+                <div className="hidden md:block">{holder.address}</div>
+                <div className="block md:hidden">{shortenAddress(holder.address)}</div>
                 {excludedAddresses.includes(holder.address) && <span className="ml-3 uppercase bg-primaryDark text-gray-800 rounded text-xs font-semibold px-2 py-1">Team Locked</span>}
                 {holder.address === zilSwapAddress && <span className="ml-3 uppercase bg-primaryDark text-gray-800 rounded text-xs font-semibold px-2 py-1">ZilSwap</span>}
               </div>
