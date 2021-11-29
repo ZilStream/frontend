cc8e80b8efe6fa5b265265e581d82db8d1b341ae
Melvin Beemer <melvinbeemer@me.com>
2021-11-29 19:38:36 +0100

    Remove notice

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -80,9 +80,6 @@ export default function Layout(props: Props) {
           <WalletModal />
           <CurrencyModal />
           <ExchangeModal />
-          <Notice title="Ongoing ZilSwap Migration">
-            ZilSwap is migrating to a new contract due to an issue found. <span className="font-bold">Liquidity has been safely stored and all funds are safe.</span> <a className="underline" href="https://twitter.com/ZilSwap">Visit ZilSwap's Twitter for the latest updates.</a>
-          </Notice>
           <div className="px-3 md:px-4 my-4 flex-grow">{children}</div>
           {/* {router.pathname !== '/swap' &&
             <>
