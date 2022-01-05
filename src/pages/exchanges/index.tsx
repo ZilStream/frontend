d339679dd16a1aae9d88f5b23f928c7220f84370
Melvin Beemer <melvinbeemer@me.com>
2022-01-05 22:23:14 +0700

    Updated exchanges ui

--- src/pages/exchanges/index.tsx
+++ src/pages/exchanges/index.tsx
@@ -41,7 +41,37 @@ const Exchanges = ({ exchanges }: InferGetServerSidePropsType<typeof getServerSi
           </div>
         </div>
       </div>      
-      <div className="scrollable-table-container max-w-full overflow-x-scroll">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        {exchanges.map(exchange => {
+          return (
+            <div className="bg-white dark:bg-gray-800 rounded-lg p-5">
+              <div className="flex items-center gap-3">
+                <div className="w-10 h-10">
+                  <TokenIcon url={exchange.icon} />
+                </div>
+                <div className="font-semibold text-xl">
+                  {exchange.name}
+                </div>
+              </div>
+              <div className="grid grid-cols-3 text-sm mt-4">
+                <div>
+                  <div className="text-gray-500 dark:text-gray-400"># of Markets</div>
+                  <div>{exchange.pairs.length}</div>
+                </div>
+                <div>
+                  <div className="text-gray-500 dark:text-gray-400">Liquidity</div>
+                  <div>—</div>
+                </div>
+                <div>
+                  <div className="text-gray-500 dark:text-gray-400">Volume</div>
+                  <div>—</div>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      {/* <div className="scrollable-table-container max-w-full overflow-x-scroll">
         <table className="zilstream-table table-fixed border-collapse">
           <colgroup>
             <col style={{width: '250px', minWidth: 'auto'}} />
@@ -85,7 +115,7 @@ const Exchanges = ({ exchanges }: InferGetServerSidePropsType<typeof getServerSi
             })}
           </tbody>
         </table>
-      </div>
+      </div> */}
     </>
   )
 }
