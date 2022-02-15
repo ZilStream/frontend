dcdb86836e8d4c644d007f4c65fdd8f8196a4dc0
Melvin Beemer <melvinbeemer@me.com>
2022-02-15 23:53:55 +0700

    Fix get transactiosn bug and add date range

--- src/components/TransactionsExportModal.tsx
+++ src/components/TransactionsExportModal.tsx
@@ -1,5 +1,4 @@
 import { Dialog, Transition } from '@headlessui/react'
-import TokenIcon from 'components/TokenIcon'
 import React, { Fragment, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { openExport } from 'store/modal/actions'
@@ -16,8 +15,8 @@ const TransactionsExportModal = () => {
   const dispatch = useDispatch()
   const [isOpen, setIsOpen] = useState(false)
   const [transactions, setTransactions] = useState<Transaction[]>([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [{from, to}, setRange] = useState<{from: Date|undefined, to: Date|undefined}>({from: undefined, to: undefined})
+  const [isLoading, setIsLoading] = useState(false)
+  const [range, setRange] = useState<{from: dayjs.Dayjs, to: dayjs.Dayjs}>({from: dayjs().startOf('month'), to: dayjs().endOf('day')})
 
   useEffect(() => {
     dispatch(openExport(isOpen))
@@ -28,9 +27,10 @@ const TransactionsExportModal = () => {
     setIsOpen(modalState.exportOpen)
   }, [modalState.exportOpen])
 
-  useEffect(() => {
+  const handleGetTransactions = () => {
+    setIsLoading(true)
     fetchTransactions()
-  }, [])
+  }
 
   const fetchTransactions = async () => {
     if(!accountState.selectedWallet) return
@@ -38,17 +38,15 @@ const TransactionsExportModal = () => {
     var totalTransactions: any[] = []
 
     var hasTransactions = true
-    var currentPage = 0
+    var currentPage = 1
     while (hasTransactions) {
-      const txns = await getTransactions(accountState.selectedWallet.address, currentPage)
+      const txns = await getTransactions(accountState.selectedWallet.address, currentPage, 20, range.from.format('YYYY-MM-DD'), range.to.format('YYYY-MM-DD'))
       totalTransactions.push(...txns.data)
-
-      currentPage += 1
-
-      console.log(currentPage, txns.pages)
 
       if(currentPage === txns.pages || currentPage > txns.pages) {
         hasTransactions = false
+      } else {
+        currentPage += 1
       }
     }
 
@@ -59,6 +57,8 @@ const TransactionsExportModal = () => {
 
     setTransactions(totalTransactions)
     setIsLoading(false)
+
+    console.log(`Found transactions: ${totalTransactions.length}`)
   }
 
   return (
@@ -80,14 +80,7 @@ const TransactionsExportModal = () => {
             <div className="relative bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 py-4 px-5 my-4 flex flex-col rounded-lg shadow-lg w-110 max-w-full mx-auto">
               <Dialog.Title className="text-lg font-bold mb-1">Export your transactions <span className="bg-primary rounded text-sm py-1 px-2">Beta</span></Dialog.Title>
 
-              {isLoading &&
-                <div className="flex items-center gap-2 text-sm text-gray-500 dark:text-gray-400">
-                  <LoadingIndicator />
-                  Retrieving all your transactions..
-                </div>
-              }
-
-              {/* <div className="flex-grow overflow-y-scroll border-t dark:border-gray-700 mt-2 pt-2">
+              <div className="flex-grow border-t dark:border-gray-700 mt-2 pt-2">
                 <div className="text-sm font-medium mb-1">Range</div>
                 <div className="grid grid-cols-2 gap-4 mb-4">
                   <input
@@ -95,6 +88,8 @@ const TransactionsExportModal = () => {
                     className="bg-gray-200 dark:bg-gray-800 focus:outline-none focus:ring-1 focus:ring-gray-300 dark:focus:ring-gray-700 rounded py-3 px-3 text-sm"
                     min="2019-01-31"
                     max={dayjs().format('YYYY-MM-DD')}
+                    onChange={e => setRange({...range, from: dayjs(e.target.value)})}
+                    value={range.from.format('YYYY-MM-DD')}
                   />
 
                   <input
@@ -102,17 +97,36 @@ const TransactionsExportModal = () => {
                     className="bg-gray-200 dark:bg-gray-800 focus:outline-none focus:ring-1 focus:ring-gray-300 dark:focus:ring-gray-700 rounded py-3 px-3 text-sm"
                     min="2019-01-31"
                     max={dayjs().format('YYYY-MM-DD')}
+                    onChange={e => setRange({...range, to: dayjs(e.target.value)})}
+                    value={range.to.format('YYYY-MM-DD')}
                   />
                 </div>
-              </div> */}
+              </div>
 
-              <CSVLink 
-                data={transactions}
-                filename={'transactions.csv'}
-                className="bg-primary rounded-lg px-3 py-2 text-center"
+
+              <button
+                className={`${transactions.length > 0 ? 'bg-gray-300 dark:bg-gray-700' : 'bg-primary'} rounded-lg px-3 py-2 text-center font-semibold mb-2`}
+                onClick={() => handleGetTransactions()}
               >
-                Download CSV
-              </CSVLink>
+                Get Transactions
+              </button>
+
+              {isLoading &&
+                <div className="flex items-center gap-2 text-sm text-gray-500 dark:text-gray-400">
+                  <LoadingIndicator />
+                  Retrieving all your transactions..
+                </div>
+              }
+              
+              {transactions.length > 0 &&
+                <CSVLink 
+                  data={transactions}
+                  filename={'transactions.csv'}
+                  className="bg-primary rounded-lg px-3 py-2 text-center"
+                >
+                  Download CSV
+                </CSVLink>
+              }
             </div>
           </Transition.Child>
         </div>
