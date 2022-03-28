b43c13b04abffe189b2cecd0fbe98b3ceae83245
Melvin Beemer <melvinbeemer@me.com>
2022-03-28 08:35:42 +0700

    Update exchange endpoints

--- src/lib/zilstream/getExchange.ts
+++ src/lib/zilstream/getExchange.ts
@@ -1,6 +1,6 @@
 import { Exchange } from "types/exchange.interface"
 
 export default async function getExchange(slug: string): Promise<Exchange>  {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/v2/exchanges/${slug}`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_IO_URL}/exchanges/${slug}`)
   return await res.json()
 }
