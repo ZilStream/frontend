c0366afaef798d81d6c881000fecc43904670e01
Melvin Beemer <melvinbeemer@me.com>
2022-03-28 11:45:55 +0700

    Swap TVL graph

--- src/lib/zilstream/getTVL.ts
+++ src/lib/zilstream/getTVL.ts
@@ -1,6 +1,6 @@
 import { ChartDataPoint } from "components/Chart";
 
 export default async function getTVL(): Promise<{time: string, value: number}[]> {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/tvl?period=month`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_IO_URL}/chart/tvl`)
   return res.json()
 }
