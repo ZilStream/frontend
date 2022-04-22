3f58cee7cd068ce486bdaa1305133ac41a9987ff
Melvin Beemer <melvinbeemer@me.com>
2022-04-22 18:35:01 +0700

    Fix HUNY banner

--- src/components/SponsorBlock.tsx
+++ src/components/SponsorBlock.tsx
@@ -11,7 +11,7 @@ const SponsorBlock = (props: Props) => {
   
   return (
     <a href={`${link ?? ''}?ref=zilstream`} target="_blank">
-      <div className={`h-48 rounded-xl py-2 px-3 shadow bg-white dark:bg-gray-800 text-white dark:text-white relative flex flex-col bg-center bg-cover bg-[url('https://zilstream.b-cdn.net/sponsorships/soulless-citadel.jpg')]`}>
+      <div className={`h-48 rounded-xl py-2 px-3 shadow bg-white dark:bg-gray-800 text-white dark:text-white relative flex flex-col bg-center bg-cover bg-[url('https://zilstream.b-cdn.net/sponsorships/zolar.png')]`}>
         <div className="mb-2">
           <div className="flex items-center text-lg -mb-1">
             <div className="flex-grow flex items-center">
@@ -28,8 +28,8 @@ const SponsorBlock = (props: Props) => {
             <Tippy content={<div className="bg-white dark:bg-gray-700 border dark:border-gray-800 px-4 py-3 rounded-lg shadow-md text-sm">
               ZilStream sponsorships directly support the ongoing development and maintenance costs of the site.
             </div>}>
-              <button className="ml-2 focus:outline-none flex items-centered gap-1 text-gray-800 opacity-75">
-                <Info size={14} className="text-gray-800" /> Sponsored
+              <button className="ml-2 focus:outline-none flex items-centered gap-1 text-gray-100 opacity-75">
+                <Info size={14} className="text-gray-100" /> Sponsored
               </button>
             </Tippy>
           </div>
