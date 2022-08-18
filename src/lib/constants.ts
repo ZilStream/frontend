c9ecf817a97060a1749e295df01ea95efaece8b7
Melvin Beemer <melvinbeemer@me.com>
2022-08-18 11:27:08 +0700

    Update XCADDEX implementation address

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,6 +3,6 @@ export const STREAM_ADDRESS = "zil1504065pp76uuxm7s9m2c4gwszhez8pu3mp6r8c"
 export const CARB_ADDRESS = "zil1hau7z6rjltvjc95pphwj57umdpvv0d6kh2t8zk"
 
 export const ZILSWAP_ADDRESS = "zil1gkwt95a67lnpe774lcmz72y6ay4jh2asmmjw6u"
-export const XCADDEX_ADDRESS = "zil1r7c6fltm4993v9myr4sz9wjgeta80mhs59sq67"
+export const XCADDEX_ADDRESS = "zil1655cde9yp0r77jdjfqnh95cpwu38nf6tn95kmd"
 export const CARBSWAP_ADDRESS = "zil1m8wfldkkm6spqdmg4ws269772a8q9wlzl8nr9u"
 export const ZILALL_ADDRESS = "zil19yf3xsn9780ggjlh48cmagyet6ex77vv7rt5xp"
