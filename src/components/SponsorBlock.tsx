34eae3edeb4027455fb062d5a3fc5f3ebbd27b2f
Melvin Beemer <melvinbeemer@me.com>
2022-09-17 14:35:35 +0700

    Update sponsorship

--- src/components/SponsorBlock.tsx
+++ src/components/SponsorBlock.tsx
@@ -11,7 +11,7 @@ const SponsorBlock = (props: Props) => {
   
   return (
     <a href={`${link ?? ''}?ref=zilstream`} target="_blank">
-      <div className={`h-48 rounded-xl py-2 px-3 shadow bg-white dark:bg-gray-800 text-white dark:text-white relative flex flex-col bg-center bg-cover bg-[url('https://zilstream.b-cdn.net/sponsorships/dxfees.jpg')]`}>
+      <div className={`h-48 rounded-xl py-2 px-3 shadow bg-white dark:bg-gray-800 text-white dark:text-white relative flex flex-col bg-center bg-cover bg-[url('https://zilstream.b-cdn.net/sponsorships/ludo-tournament.jpg')]`}>
         <div className="mb-2">
           <div className="flex items-center text-lg -mb-1">
             <div className="flex-grow flex items-center">
@@ -22,7 +22,7 @@ const SponsorBlock = (props: Props) => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-end justify-end h-full gap-4 text-sm -mt-1">
+        <div className="flex flex-col items-end justify-start h-full gap-4 text-sm -mt-1">
           <div className="flex items-center text-xs text-gray-400 dark:text-gray-400">
             {/* Provided by ZilStream */}
             <Tippy content={<div className="bg-white dark:bg-gray-700 border dark:border-gray-800 px-4 py-3 rounded-lg shadow-md text-sm">
