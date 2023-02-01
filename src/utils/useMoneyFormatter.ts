6c3e14338bc2d41e76c42f892665dc13f7cc07a1
Melvin Beemer <melvinbeemer@me.com>
2023-02-01 08:50:12 +0700

    Format formatter

--- src/utils/useMoneyFormatter.ts
+++ src/utils/useMoneyFormatter.ts
@@ -10,20 +10,28 @@ export type MoneyFormatterOptions = {
   maxFractionDigits?: number;
   showCurrency?: boolean;
   toHumanNumber?: boolean;
-}
+};
 
-export const toBigNumber = (inputNumber: BigNumber | number | string = 0, opts: MoneyFormatterOptions = {}): BigNumber => {
+export const toBigNumber = (
+  inputNumber: BigNumber | number | string = 0,
+  opts: MoneyFormatterOptions = {}
+): BigNumber => {
   if (typeof inputNumber === "string") inputNumber = Number(inputNumber);
   if (typeof inputNumber === "number") {
-    if (isNaN(inputNumber) || !isFinite(inputNumber))
-      return new BigNumber(0)
+    if (isNaN(inputNumber) || !isFinite(inputNumber)) return new BigNumber(0);
   }
   let number = new BigNumber(inputNumber);
   if (isNaN(number.toNumber())) number = new BigNumber(0);
-  let { currency, symbol, compression = 0, decPlaces, maxFractionDigits = 2, showCurrency = false } = opts;
+  let {
+    currency,
+    symbol,
+    compression = 0,
+    decPlaces,
+    maxFractionDigits = 2,
+    showCurrency = false,
+  } = opts;
 
-  if (decPlaces === undefined)
-    decPlaces = maxFractionDigits || 0;
+  if (decPlaces === undefined) decPlaces = maxFractionDigits || 0;
 
   if (currency && !compression) {
     const defaultCurrency = active();
@@ -33,21 +41,29 @@ export const toBigNumber = (inputNumber: BigNumber | number | string = 0, opts: 
     symbol = currencyData.symbol;
   }
   number = number.shiftedBy(-compression);
-  return number
-}
+  return number;
+};
 
-const formatter = (inputNumber: BigNumber | number | string = 0, opts: MoneyFormatterOptions = {}): string => {
+const formatter = (
+  inputNumber: BigNumber | number | string = 0,
+  opts: MoneyFormatterOptions = {}
+): string => {
   if (typeof inputNumber === "string") inputNumber = Number(inputNumber);
   if (typeof inputNumber === "number") {
-    if (isNaN(inputNumber) || !isFinite(inputNumber))
-      return `${inputNumber}`;
+    if (isNaN(inputNumber) || !isFinite(inputNumber)) return `${inputNumber}`;
   }
   let number = new BigNumber(inputNumber);
   if (isNaN(number.toNumber())) number = new BigNumber(0);
-  let { currency, symbol, compression = 0, decPlaces, maxFractionDigits = 2, showCurrency = false } = opts;
+  let {
+    currency,
+    symbol,
+    compression = 0,
+    decPlaces,
+    maxFractionDigits = 2,
+    showCurrency = false,
+  } = opts;
 
-  if (decPlaces === undefined)
-    decPlaces = maxFractionDigits || 0;
+  if (decPlaces === undefined) decPlaces = maxFractionDigits || 0;
 
   if (currency && !compression) {
     const defaultCurrency = active();
@@ -59,17 +75,22 @@ const formatter = (inputNumber: BigNumber | number | string = 0, opts: MoneyForm
   number = number.shiftedBy(-compression);
 
   if (opts.toHumanNumber) {
-    return `${toHumanNumber(number, decPlaces)}${showCurrency ? ` ${symbol ?? currency ?? ""}` : ""}`.trim()
+    return `${toHumanNumber(number, decPlaces)}${
+      showCurrency ? ` ${symbol ?? currency ?? ""}` : ""
+    }`.trim();
   }
 
   if (decPlaces <= 2) {
-    return number.toFixed(decPlaces).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')  
+    return number.toFixed(decPlaces).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
   }
-  return number.toFixed(decPlaces)
+  return number.toFixed(decPlaces);
 };
 
 export default function useMoneyFormatter(options: MoneyFormatterOptions = {}) {
-  return (number: number | string | undefined | BigNumber, adhocOptions: MoneyFormatterOptions = {}) => {
+  return (
+    number: number | string | undefined | BigNumber,
+    adhocOptions: MoneyFormatterOptions = {}
+  ) => {
     return formatter(number, { ...options, ...adhocOptions });
-  }
-};
+  };
+}
