2c99d3de61bb597690989adc6b60da3359c32025
GAEAlimited <69316708+GAEAlimited@users.noreply.github.com>
2024-06-13 16:39:01 -0400

    Update index.tsx

--- src/pages/updates/index.tsx
+++ src/pages/updates/index.tsx
@@ -7,20 +7,20 @@ function Updates() {
   return (
     <>
       <Head>
-        <title>Updates | ZilStream</title>
-        <meta property="og:title" content="Updates | ZilStream" />
+        <title>Updates | GSCxBT.com</title>
+        <meta property="og:title" content="Updates | GSCxBT.com" />
       </Head>
       <div className="py-12">
         <div className="bg-white dark:bg-gray-800 p-6 sm:p-10 rounded-lg max-w-3xl mx-auto text-lg">
           <Image 
             src="/images/zilstream-banner.png"
-            alt="ZilStream"
+            alt="GSCxBT.com"
             width={800}
             height={385}
             className="rounded-lg"
           />
-          <h1 className="mt-4 text-2xl mb-1">Announcing Premium Membership</h1>
-          <p>Introducing ZilStream Premium Membership, Portfolio and STREAM token.</p>
+          <h1 className="mt-4 text-2xl mb-1">Introducing Network Membership</h1>
+          <p>Introducing GSCxBT.com SuperChain GSC20 Membership, GAEAos Gateway Portal and XBTA.</p>
           <Link href="/updates/announcing-premium-membership">
             <a className="block mt-1 font-normal underline">Read more</a>
           </Link>
